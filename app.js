46e310a8d3e12facf6cf97f3aa9d1a081eeb4416
Ronmor <Ronmor35@gmail.com>
2020-08-26 19:47:57 +0300

    added viewer & display

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const database = require('./toDoModel');
 const app = express();
 var server = require('http').createServer(app);
 const io = require("socket.io")(server);
@@ -7,7 +6,6 @@ const port = 3000
 
 
 
-database.initializeMongo;
 //------------ kafka------------
 const kafka = require('./kafkaProduce');
 const bodyParser = require('body-parser');
@@ -17,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //------------
 //app.set('views', path.join(__dirname, 'views'));
-//app.set('views', './views');
+app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
@@ -31,9 +29,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.get('/', (req, res) => res.send("<a href='/send'>Send</a> <br/><a href=''>View</a>"));
-app.get('/send', (req, res) => res.render('sender'));
-app.get('/view', (req, res) => res.render('viewer'));
+app.get('/', (req,res)=>res.send('<a href=/send>Send</a> <br/> <a href=/view>View</a> '))
+app.get('/send', (req,res)=>res.render('sender'));
+app.get('/view', (req,res)=>res.render('viewer'));
 
 
 //------------ Socket.io ----------------
@@ -50,20 +48,6 @@ io.on("connection", (socket) => {
 //
 //const client1 = new kafka.KafkaClient({kafkaHost: "localhost:9092"});
 
-
-//----------database 18 08 2020 ---------
-
-
-app.get('/testFind', function (req, res) {
-  database.ToDo.find(function (err, Todos) {
-    if (err) res.status(500).send({ error: err });
-    console.log(Todos);
-    res.json(Todos);
-  })
-})
-
-
-//------------------------------------
 
 
 server.listen(port, () => console.log(`Ariel app listening at http://localhost:${port}`));
